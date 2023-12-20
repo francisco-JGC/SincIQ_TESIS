7beb395fdb86462c4c9e691c8126cd5fa2ae73fb
Francisco Garcia <jjjchico1@gmail.com>
2023-12-19 23:43:13 -0600

    feat: Add verify_token and received_message routes

--- src/routes/whatsapp.route.ts
+++ src/routes/whatsapp.route.ts
@@ -1,8 +1,15 @@
 import { Router, type Request, type Response } from 'express'
+import { verifyToken } from '../controllers/whatsapp.controller'
 
 const router = Router()
 
 router.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+router.get('/verify_token', verifyToken)
+
+router.post('/received_message', (_req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
