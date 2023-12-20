1cf03a5936831c662cb81779c6bf38ea05038fef
Francisco Garcia <jjjchico1@gmail.com>
2023-12-19 21:42:42 -0600

    feat: Add dynamic route loading

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import * as fs from 'fs'
+import * as path from 'path'
 
 const app = express()
 app.use(express.json())
 
 const port = process.env.PORT || 3000
+
+fs.readdirSync(path.join(__dirname, 'routes')).map(async (file) => {
+  const { default: route } = await import(`./routes/${file}`)
+  const [routeName] = file.split('.')
+  app.use('/api/' + routeName, route)
+})
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
