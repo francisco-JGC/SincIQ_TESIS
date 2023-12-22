eaa74fa0d63eb9cb4c5e5f9ab8171b101dc80bb3
Francisco Garcia <jjjchico1@gmail.com>
2023-12-21 22:02:22 -0600

    feat: Add sendMessage route to whatsapp.route.ts

--- src/routes/whatsapp.route.ts
+++ src/routes/whatsapp.route.ts
@@ -2,7 +2,8 @@ import { Router } from 'express'
 import {
   verifyToken,
   receivedMessage,
-  showLog
+  showLog,
+  sendMessage
 } from '../controllers/whatsapp.controller'
 
 const router = Router()
@@ -12,5 +13,7 @@ router.get('/', verifyToken)
 router.post('/', receivedMessage)
 
 router.get('/show_log', showLog)
+
+router.post('/send_message', sendMessage)
 
 export default router
