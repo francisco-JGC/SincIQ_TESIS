0a7c3049e0e6b63b0838f3ee331e6d7807ece998
Francisco Garcia <jjjchico1@gmail.com>
2023-12-25 20:11:08 -0600

    feat: Add WhatsApp interface and related types

---
+++ src/interfaces/whatsapp/whatsapp.interface.ts
@@ -0,0 +1,61 @@
+export interface IWhatsappReply {
+  object: string
+  entry: Entry[]
+}
+
+interface Entry {
+  id: string
+  changes: Change[]
+}
+
+interface Change {
+  value: Value
+  field: string
+}
+
+interface Value {
+  messaging_product: string
+  metadata: Metadata
+  contacts: Contact[]
+  messages: IMessage[]
+  profile: any
+}
+
+export interface IMessage {
+  from: string
+  id: string
+  timestamp: string
+  type: string
+  interactive: IInteractive
+  text: { body: string }
+}
+
+export interface IInteractive {
+  type: string
+  list_reply: Listreply
+  button_reply: Buttonreply
+}
+
+interface Listreply {
+  id: string
+  title: string
+}
+
+interface Buttonreply {
+  id: string
+  title: string
+}
+
+interface Contact {
+  profile: IProfile
+  wa_id: string
+}
+
+export interface IProfile {
+  name: string
+}
+
+interface Metadata {
+  display_phone_number: string
+  phone_number_id: string
+}
