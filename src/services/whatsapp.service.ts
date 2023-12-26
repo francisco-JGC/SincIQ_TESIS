cce5002f4fa4145e96f4af1ad6f0f227cf81457a
Francisco Garcia <jjjchico1@gmail.com>
2023-12-25 20:12:06 -0600

    feat: Add sendInteractiveButton function to send text messages with interactive buttons

--- src/services/whatsapp.service.ts
+++ src/services/whatsapp.service.ts
@@ -1,3 +1,4 @@
+import type { IInteractiveButton } from '../interfaces/whatsapp/interactiveButtons.interface'
 import {
   handleBadRequestResponse,
   handleOkResponse
@@ -19,6 +20,57 @@ export const sendTextMessage = async ({
       body: textResponse
     },
     type: 'text'
+  })
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.WHATSAAP_TOKEN}`
+    },
+    body: data
+  }
+
+  try {
+    return handleOkResponse(await useFetch(BASE_URL, options))
+  } catch (error) {
+    return handleBadRequestResponse({}, error as Error)
+  }
+}
+
+export const sendInteractiveButton = async ({
+  textResponse,
+  phone,
+  buttons
+}: {
+  textResponse: string
+  phone: string
+  buttons: IInteractiveButton[]
+}) => {
+  if (buttons.length > 3) {
+    return handleBadRequestResponse(
+      {},
+      new Error('You can only send up to 3 buttons')
+    )
+  }
+
+  if (buttons.length < 1) {
+    return handleBadRequestResponse(
+      {},
+      new Error('You must send at least 1 button')
+    )
+  }
+
+  const data = JSON.stringify({
+    messaging_product: 'whatsapp',
+    recipient_type: 'individual',
+    to: phone,
+    type: 'interactive',
+    interactive: {
+      type: 'button',
+      text: textResponse,
+      action: buttons
+    }
   })
 
   const options = {
