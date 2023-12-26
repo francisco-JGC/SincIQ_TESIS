b6206b8ae337bade586997fa359b9fdfd8a574f2
Francisco Garcia <jjjchico1@gmail.com>
2023-12-25 20:12:16 -0600

    fix: Remove showLog route from whatsapp.route.ts

--- src/routes/whatsapp.route.ts
+++ src/routes/whatsapp.route.ts
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import {
   verifyToken,
   receivedMessage,
-  showLog,
   sendMessage
 } from '../controllers/whatsapp.controller'
 
@@ -11,8 +10,6 @@ const router = Router()
 router.get('/', verifyToken)
 
 router.post('/', receivedMessage)
-
-router.get('/show_log', showLog)
 
 router.post('/send_message', sendMessage)
 
