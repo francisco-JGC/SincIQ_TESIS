7af9bc0de1296aee907b9316ff4ff38335c28ee4
Francisco Garcia <jjjchico1@gmail.com>
2023-12-28 01:51:40 -0600

    feat: Add clients route and controller for creating clients

---
+++ src/routes/clients.route.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import { createClient } from '../controllers/clients.cotroller'
+import { handleBadRequestResponse } from '../utils/handleHttpsResponse'
+import { ICreateClient } from '../entities/client/types/add-client.interface'
+
+const router = Router()
+
+router.post('/', async (req, res) => {
+  const { username, phone_number } = req.body as ICreateClient
+
+  if (!username || !phone_number) {
+    return handleBadRequestResponse(res, new Error('No client data provided'))
+  }
+
+  return res.json(await createClient(username, phone_number))
+})
+
+export default router
