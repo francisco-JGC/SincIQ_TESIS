419bf8aad4c03b05c17f0d42c957730cda8cc574
Francisco Garcia <jjjchico1@gmail.com>
2023-12-28 01:49:45 -0600

    feat: Add database configuration file

---
+++ src/config/database.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import { DataSource } from 'typeorm'
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  logging: true,
+  synchronize: true,
+  entities: ['src/entities/**/*.entity.ts']
+})
