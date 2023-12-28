2367ee4ae133a0c8911b0b9100e89625d1f39df2
Francisco Garcia <jjjchico1@gmail.com>
2023-12-28 17:30:35 -0600

    feat: Add message controller for creating messages

---
+++ src/controllers/message.controller.ts
@@ -0,0 +1,48 @@
+import { Message } from '../entities/message/message.entity'
+import { Conversation } from '../entities/conversation/conversation.entity'
+import { AppDataSource } from '../config/database.config'
+import {
+  handleBadRequestResponse,
+  handleOkResponse
+} from '../utils/handleHttpsResponse'
+import { getConversationWithSystem } from './conversation.controller'
+import { getClientByPhoneNumber } from './clients.cotroller'
+import { Client } from '../entities/client/clients.entity'
+
+export const createMessage = async (
+  message: string,
+  phone_number: string,
+  receiver: string
+) => {
+  try {
+    const dataClient = await getClientByPhoneNumber(phone_number)
+
+    if (!dataClient.success) {
+      return handleBadRequestResponse({}, new Error('Client not found'))
+    }
+
+    const client = dataClient.data as Client
+
+    const conversation = await getConversationWithSystem(client.phone_number)
+
+    const messageObject = new Message()
+    messageObject.content = message
+
+    if (receiver === 'system') {
+      messageObject.sender = client.phone_number
+      messageObject.receiver = 'system'
+    } else {
+      messageObject.sender = 'system'
+      messageObject.receiver = client.phone_number
+    }
+
+    messageObject.conversation = conversation.data as Conversation
+
+    const newMessage =
+      await AppDataSource.getRepository(Message).save(messageObject)
+
+    return handleOkResponse(newMessage)
+  } catch (error: any) {
+    return handleBadRequestResponse({}, error.message)
+  }
+}
