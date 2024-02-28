74ac2a0756807a74082c7d386fb697d733a7be82
Francisco Garcia <jjjchico1@gmail.com>
2024-02-28 00:50:04 -0600

    Add register route to auth.route.ts

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { login } from '../controllers/auth.controller'
+import { login, register } from '../controllers/auth.controller'
 
 const router = Router()
 
 router.post('/login', login)
+
+router.post('/register', register)
 
 export default router
