4acab8b9ff3ed20673f77a167e2cd1eff8b2a1d9
Francisco Garcia <jjjchico1@gmail.com>
2024-03-14 23:36:03 -0600

    Add active-desactive-bot endpoint and interface

---
+++ src/entities/client/types/active-desactive-bot.ts
@@ -0,0 +1,4 @@
+export interface IActiveDesactiveBot {
+  client_id: number
+  bot_status: boolean
+}
--- src/routes/clients.route.ts
+++ src/routes/clients.route.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express'
-import { createClient, getClients } from '../controllers/clients.cotroller'
+import {
+  createClient,
+  getClients,
+  changeBotStatus
+} from '../controllers/clients.cotroller'
 import { handleBadRequestResponse } from '../utils/handleHttpsResponse'
 import { ICreateClient } from '../entities/client/types/add-client.interface'
+import { IActiveDesactiveBot } from '../entities/client/types/active-desactive-bot'
 
 const router = Router()
 
@@ -18,5 +23,20 @@ router.post('/', async (req, res) => {
 })
 
 router.get('/', getClients)
+
+router.post('/active-desactive-bot', async (req, res) => {
+  const { client_id, bot_status } = req.body as IActiveDesactiveBot
+
+  if (!client_id || bot_status === undefined) {
+    return res.json(
+      handleBadRequestResponse(
+        res,
+        new Error('Por favor, envie os dados corretos')
+      )
+    )
+  }
+
+  return res.json(await changeBotStatus(client_id, bot_status))
+})
 
 export default router
