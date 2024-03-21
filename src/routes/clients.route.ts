7ed007311126293ff88f8840bcc7cd890e9047e7
Francisco Garcia <jjjchico1@gmail.com>
2024-03-20 23:52:07 -0600

    Add clearConversationsFromClient route handler

--- src/routes/clients.route.ts
+++ src/routes/clients.route.ts
@@ -2,7 +2,8 @@ import { Router } from 'express'
 import {
   createClient,
   getClients,
-  changeBotStatus
+  changeBotStatus,
+  clearConversationsFromClient
 } from '../controllers/clients.cotroller'
 import { handleBadRequestResponse } from '../utils/handleHttpsResponse'
 import { ICreateClient } from '../entities/client/types/add-client.interface'
@@ -22,7 +23,9 @@ router.post('/', async (req, res) => {
   return res.json(await createClient(username, phone_number))
 })
 
-router.get('/', getClients)
+router.get('/', async (_req, res) => {
+  return res.json(await getClients())
+})
 
 router.post('/change-bot-status', async (req, res) => {
   const { client_id, bot_status } = req.body as IActiveDesactiveBot
@@ -37,6 +40,21 @@ router.post('/change-bot-status', async (req, res) => {
   }
 
   return res.json(await changeBotStatus(client_id, bot_status))
+})
+
+router.post('/clear-conversation', async (req, res) => {
+  const { client_id } = req.body as IActiveDesactiveBot
+
+  if (!client_id) {
+    return res.json(
+      handleBadRequestResponse(
+        res,
+        new Error('Por favor, envie los datos necesarios')
+      )
+    )
+  }
+
+  return res.json(await clearConversationsFromClient(client_id))
 })
 
 export default router
