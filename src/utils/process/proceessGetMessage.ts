ce7853456b34077d19559fc312821b4d89cc1562
Francisco Garcia <jjjchico1@gmail.com>
2024-03-26 01:18:25 -0600

    Delete processGetMessage.ts

--- src/utils/process/proceessGetMessage.ts
+++
@@ -1,9 +0,0 @@
-export interface IGetMessage {
-  text: (params: any) => string
-  image: (params: any) => string
-}
-
-export const processGetMessage: IGetMessage = {
-  text: (params) => params.text.body,
-  image: (params) => params.image.caption
-}
