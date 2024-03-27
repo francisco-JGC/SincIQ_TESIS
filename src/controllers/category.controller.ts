01bf8419333a44f50662f687854a1c8ed9645665
Francisco Garcia <jjjchico1@gmail.com>
2024-03-26 23:57:43 -0600

    Add category controller with CRUD operations

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,113 @@
+import { AppDataSource } from '../config/database.config'
+import { Category } from '../entities/products/category.entity'
+import { ICreateCategory } from '../entities/products/types/category/create-category'
+import {
+  handleBadRequestResponse,
+  handleOkResponse
+} from '../utils/handleHttpsResponse'
+
+export const createCategory = async ({
+  name,
+  description
+}: ICreateCategory) => {
+  try {
+    const category = new Category()
+    category.name = name
+    category.description = description
+
+    const result = await AppDataSource.getRepository(Category).save(category)
+
+    if (!result) {
+      return handleBadRequestResponse({}, 'Hubo un error al crear la categoría')
+    }
+
+    return handleOkResponse(result)
+  } catch (error: any) {
+    return handleBadRequestResponse({}, error.message)
+  }
+}
+
+export const getCategories = async () => {
+  try {
+    const categories = await AppDataSource.getRepository(Category).find()
+
+    if (!categories) {
+      return handleBadRequestResponse({}, 'No se encontraron categorías')
+    }
+
+    return handleOkResponse(categories)
+  } catch (error: any) {
+    return handleBadRequestResponse({}, error.message)
+  }
+}
+
+export const getCategoryById = async (id: number) => {
+  try {
+    const category = await AppDataSource.getRepository(Category).findOne({
+      where: { id }
+    })
+
+    if (!category) {
+      return handleBadRequestResponse({}, 'No se encontró la categoría')
+    }
+
+    return handleOkResponse(category)
+  } catch (error: any) {
+    return handleBadRequestResponse({}, error.message)
+  }
+}
+
+export const updateCategory = async (id: number, data: ICreateCategory) => {
+  try {
+    const category = await AppDataSource.getRepository(Category).findOne({
+      where: { id }
+    })
+
+    if (!category) {
+      return handleBadRequestResponse({}, 'No se encontró la categoría')
+    }
+
+    category.name = data.name
+    category.description = data.description
+
+    const result = await AppDataSource.getRepository(Category).save(category)
+
+    if (!result) {
+      return handleBadRequestResponse(
+        {},
+        'Hubo un error al actualizar la categoría'
+      )
+    }
+
+    return handleOkResponse(result)
+  } catch (error: any) {
+    return handleBadRequestResponse({}, error.message)
+  }
+}
+
+export const deleteCategory = async (id: number) => {
+  try {
+    const category = await AppDataSource.getRepository(Category).findOne({
+      where: { id }
+    })
+
+    if (!category) {
+      return handleBadRequestResponse({}, 'No se encontró la categoría')
+    }
+
+    const result = await AppDataSource.getRepository(Category).delete({
+      id
+    })
+
+    if (!result) {
+      return handleBadRequestResponse(
+        {},
+        'Hubo un error al eliminar la categoría'
+      )
+    }
+
+    return handleOkResponse(result)
+  } catch (error: any) {
+    return handleBadRequestResponse({}, error.message)
+  }
+}
