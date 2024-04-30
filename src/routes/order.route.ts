b8903c3dc6c3d3df1458223d823b35d044267726
Francisco Garcia <jjjchico1@gmail.com>
2024-04-30 10:31:05 -0600

    Refactor order.route.ts to handle order creation endpoint

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,28 +1,20 @@
 import { Router } from 'express'
 import { createInstantOrder } from '../controllers/orders.controller'
 import { ICreateInstantOrder } from '../entities/order/types/create-order'
-import {
-  handleBadRequestResponse,
-  handleOkResponse
-} from '../utils/handleHttpsResponse'
+import { handleBadRequestResponse } from '../utils/handleHttpsResponse'
 
 const router = Router()
 
 router.post('/', async (req, res) => {
-  const { order } = req.body as { order: ICreateInstantOrder }
+  const order = req.body as ICreateInstantOrder
 
-  if (
-    !order.client_name ||
-    !order.id_products ||
-    !order.quantity ||
-    !order.total_price
-  ) {
+  if (!order.id_products || !order.total_price) {
     return res.json(
       handleBadRequestResponse({}, new Error('Faltan datos de la orden'))
     )
   }
 
-  return handleOkResponse(await createInstantOrder(order))
+  return res.json(await createInstantOrder(order))
 })
 
 export default router
