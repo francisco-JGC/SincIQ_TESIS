6e7da79289a74697f5694c2c6a51907c2145b024
Francisco Garcia <jjjchico1@gmail.com>
2024-05-01 22:54:43 -0600

    Refactor createInstantOrder function to improve performance and readability

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,5 +1,5 @@
 import { Order } from '../entities/order/order.entity'
-// import { Client } from '../entities/client/clients.entity'
+import { Client } from '../entities/client/clients.entity'
 import { Product } from '../entities/products/products.entity'
 import { AppDataSource } from '../config/database.config'
 import {
@@ -40,14 +40,38 @@ export const createInstantOrder = async (order: ICreateInstantOrder) => {
       newOrder.products.push(product)
     }
 
-    await AppDataSource.getRepository(Order)
-      .createQueryBuilder()
-      .insert()
-      .into(Order)
-      .values(newOrder)
-      .execute()
+    const response = await AppDataSource.getRepository(Order).save(newOrder)
 
+    console.log(response)
     return handleOkResponse(newOrder)
+  } catch (error: any) {
+    return handleBadRequestResponse({}, error.message)
+  }
+}
+
+export const getSaleOrders = async () => {
+  try {
+    const orders = await AppDataSource.getRepository(Order).find({
+      where: { state: 'completed' },
+      relations: ['products']
+    })
+    const product = await AppDataSource.getRepository(Product).find()
+    const client = await AppDataSource.getRepository(Client).find()
+
+    const total_sales = orders
+      .map((sale) => sale.total_price)
+      .reduce((acc, curr) => acc + curr, 0)
+
+    const total_products = product.length
+    const total_clients = client.length
+
+    const income = {
+      total_sales,
+      total_products,
+      total_clients
+    }
+
+    return handleOkResponse({ orders, income })
   } catch (error: any) {
     return handleBadRequestResponse({}, error.message)
   }
